ac08785a071e30e75d8e0289f44e99c89a0fd13b
109isaque10 <isaquevictor803@hotmail.com>
2020-09-16 19:31:50 -0300

    Fix bug

--- src/com/tigames/tiles/TileMapObj.java
+++ src/com/tigames/tiles/TileMapObj.java
@@ -17,12 +17,14 @@ public class TileMapObj extends TileMap{
         String[] block = data.split(",");
         for(int i = 0; i < (width * height); i++){
             int temp = Integer.parseInt(block[i].replaceAll("\\s+",""));
-            if(temp != 0){
-                tempBlock = new HoleBlock(sprite.getSprite((int) ((temp - 1) % tileColumns), (int) ((temp - 1) / tileColumns)),new Vector2f((int) (i % width) * tileWidth, (int) (i / height) * tileHeight),tileWidth, tileHeight);
-            }else {
-                tempBlock = new ObjBlock(sprite.getSprite((int) ((temp - 1) % tileColumns), (int) ((temp - 1) / tileColumns)), new Vector2f((int) (i % width) * tileWidth, (int) (i / height) * tileHeight), tileWidth, tileHeight);
+            if(temp != 0) {
+                if (temp == 2) {
+                    tempBlock = new HoleBlock(sprite.getSprite((int) ((temp - 1) % tileColumns), (int) ((temp - 1) / tileColumns)), new Vector2f((int) (i % width) * tileWidth, (int) (i / height) * tileHeight), tileWidth, tileHeight);
+                } else {
+                    tempBlock = new ObjBlock(sprite.getSprite((int) ((temp - 1) % tileColumns), (int) ((temp - 1) / tileColumns)), new Vector2f((int) (i % width) * tileWidth, (int) (i / height) * tileHeight), tileWidth, tileHeight);
+                }
+                tmo_blocks.put(String.valueOf((int) (i % width)) + "," + String.valueOf((int) (i / height)), tempBlock);
             }
-            tmo_blocks.put(String.valueOf((int) (i % width)) + "," + String.valueOf((int) (i / height)), tempBlock);
         }
     }
     public void render(Graphics2D g){
