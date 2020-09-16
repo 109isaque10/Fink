91f66a367e2d219c999d0b1c02dd949b9714658b
109isaque10 <isaquevictor803@hotmail.com>
2020-09-16 18:58:19 -0300

    Fix bug

--- src/com/tigames/tiles/TileMapNorm.java
+++ src/com/tigames/tiles/TileMapNorm.java
@@ -11,13 +11,12 @@ import java.util.ArrayList;
 public class TileMapNorm extends TileMap{
     private ArrayList<Block> blocks;
     public TileMapNorm(String data, Sprite sprite, int width, int height, int tileWidth, int tileHeight, int tileColumns){
-        blocks = new ArrayList<Block>();
+        blocks = new ArrayList<>();
         String[] block = data.split(",");
         for(int i = 0; i < (width * height); i++){
             int temp = Integer.parseInt(block[i].replaceAll("\\s+",""));
             if(temp != 0)
                 blocks.add(new NormBlock(sprite.getSprite((int) ((temp - 1) % tileColumns), (int) ((temp - 1) / tileColumns)),new Vector2f((int) (i % width) * tileWidth, (int) (i / height) * tileHeight),tileWidth, tileHeight));
-
         }
     }
 
